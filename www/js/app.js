d2b82a792103e4f8df54e705381edb1eb91b8aeb
AnqurVanillapy <anqurvanillapy@gmail.com>
2016-06-20 14:03:22 +0800

    updated logo and fixed path errors

--- www/js/app.js
+++ www/js/app.js
@@ -160,7 +160,7 @@
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-      .register('/service-worker.js')
+      .register('../service-worker.js')
       .then(function () {
         console.log('Service worker registered')
       })
